4cbdfec97a4d30bc9454736980b1330400bb9a60
Andrei <Andrei@brainspire.com>
2022-08-09 14:20:10 -0600

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about pa-6">
+    <h1>About ToDo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+      architecto ipsam ullam dolor, reprehenderit ipsum, unde optio at ex, quod
+      maiores eligendi repellat molestiae eos ipsa neque et ea distinctio.
+    </p>
+  </div>
+</template>
